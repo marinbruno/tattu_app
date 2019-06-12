a18b631d49ffed0af4e9fa310f3e8e356eddce31
Bruno <bruno_marin@hotmail.com>
2019-06-12 10:57:14 -0300

    added photos to seeds.rb and fixed a few things in the models

--- app/models/tattoo.rb
+++ app/models/tattoo.rb
@@ -2,6 +2,6 @@ class Tattoo < ApplicationRecord
   belongs_to :artist
 
   has_many :taggings, dependent: :destroy
-  has_many :photos, as: :photoable, dependent: :destroy
+  has_one :photo, as: :photoable, dependent: :destroy
   has_many :tags, through: :taggings
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,7 @@ Tag.destroy_all
 Tagging.destroy_all
 
 users_array = []
-
-10.times do
+15.times do
   puts 'Creating an user...'
   user_hash = {
     name: Faker::Name.name,
@@ -29,6 +28,7 @@ end
 
 artists_array = []
 5.times do
+  puts 'Creating an artist...'
   user_hash = {
     name: Faker::Name.name,
     email: Faker::Internet.email,
@@ -36,10 +36,9 @@ artists_array = []
   }
   new_user = User.new(user_hash)
   new_user.save
-  users_array << new_user
 
   artist_hash = {
-    description: Faker::Football.position,
+    description: Faker::Sports::Football.position,
     user: new_user
   }
   new_artist = Artist.new(artist_hash)
@@ -59,12 +58,12 @@ tattoos_array = []
   tattoos_array << new_tattoo
 end
 
-tags_array = ['3D', 'Abstract', 'Ambigram', 'Black And Grey', 'Graffiti', 'Old School', 'Tribal']
-arr = []
-tags_array.each do |t|
-  tag = Tag.new({name: t})
+tags_names = ['3D', 'Abstract', 'Ambigram', 'Black And Grey', 'Graffiti', 'Old School', 'Tribal']
+tags_array = []
+tags_names.each do |tag|
+  tag = Tag.new({name: tag})
   tag.save
-  arr << tag
+  tags_array << tag
 end
 
 
@@ -72,10 +71,34 @@ taggings_array = []
 10.times do
   tagging_hash = {
     tattoo: tattoos_array.sample,
-    tag: arr.sample
+    tag: tags_array.sample
   }
   new_tagging = Tagging.new(tagging_hash)
   new_tagging.save
   taggings_array << new_tagging
 end
 
+user_photo_url = 'https://images.unsplash.com/photo-1481882466320-51765fd9fe21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+artist_photo_url = 'https://images.unsplash.com/photo-1485742032238-7543513eeba6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+tattoo_photo_url = 'https://images.unsplash.com/photo-1521308452854-e037c0062a1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=891&q=80'
+
+users_array.each do |user|
+  puts 'Saving photo to an user...'
+  user_photo = Photo.new(photo: user_photo_url)
+  user.photo = user_photo
+  user.save
+end
+
+artists_array.each do |artist|
+  puts 'Saving photo to an artist...'
+  artist_photo = Photo.new(photo: artist_photo_url)
+  artist.user.photo = artist_photo
+  artist.save
+end
+
+tattoos_array.each do |tattoo|
+  puts 'Saving photo to a tattoo...'
+  tattoo_photo = Photo.new(photo: tattoo_photo_url)
+  tattoo.photo = tattoo_photo
+  tattoo.save
+end
