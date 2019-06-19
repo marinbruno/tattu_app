3745e613395b7f7aedb08b78c6451d045e8cd4b5
Felipe Rocha <felipewp200@gmail.com>
2019-06-19 17:24:42 -0300

    tag-color

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home], raise: false
 
   def home
-    @tattoos = Tattoo.all
+    @tags = Tag.all
+    @tag = @tags.sample
   end
 end
