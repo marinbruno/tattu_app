3d70716ec9edf3d117ed07b5250b03735d9458ae
Daniel Belarmino <belarba@gmail.com>
2019-06-19 17:09:11 -0300

    tattoos list

--- app/javascript/components/infitine_scroll.js
+++ app/javascript/components/infitine_scroll.js
@@ -1,13 +1,48 @@
 import InfiniteScroll from 'infinite-scroll';
 
 const infScrollInit = () => {
-  // const infScroll = new InfiniteScroll( '.tattoo-cards', {
-  //   // options
-  //   path: '.pagination__next',
-  //   append: '.tattoo-relative',
-  //   history: false,
-  //   outlayer: msnry
-  // });
+  const loadButton = document.querySelector(".scroll-load-button");
+  const loadButtonHref = loadButton.getAttribute("href");
+  const loadPath = loadButtonHref.substring(0, loadButtonHref.length - 1);
+  debugger
+
+  const nextPage = () => {
+    const loadButtonHref = loadButton.getAttribute("href");
+    return parseInt(loadButtonHref.substring(loadButtonHref.length - 1, loadButtonHref.length));
+  }
+
+  const checkLoader = (event) => {
+    const documentHeight = document.body.scrollHeight;
+    const bottomDistanceFromTop = window.innerHeight + window.scrollY;
+    const distanceFromBottom = documentHeight - bottomDistanceFromTop;
+
+    const loadThreshold = 500;
+    const should_load = distanceFromBottom < loadThreshold;
+
+    if(should_load) {
+      window.removeEventListener("scroll", checkLoader);
+
+      var docParser = new DOMParser();
+      fetch(loadButton.getAttribute("href"))
+        .then(response => response.text())
+        .then(data => {
+          loadButton.setAttribute("href", `${loadPath}${nextPage() + 1}`)
+          const dom = docParser.parseFromString(data, "text/html");
+          const items = dom.querySelectorAll(".tattoo-relative");
+          console.log(items)
+          if(items.length === 0) {
+            window.colc.append(items);
+            window.addEventListener("scroll", checkLoader);
+          } else {
+            throw Error("No more tattoos")
+          }
+        }).catch((data) => {
+          console.log(data)
+        });
+    };
+  };
+
+  window.addEventListener("scroll", checkLoader)
 }
 
 
